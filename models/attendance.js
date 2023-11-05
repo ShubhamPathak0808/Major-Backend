24c31adb3b87322fcb72a85c759c3e5c509f96ed
Yash Singh Bhadoria <94733001+Yashsinghbhadoria14092002@users.noreply.github.com>
2023-11-06 00:45:39 +0530

    major: attendence backend (#3)

--- models/attendance.js
+++ models/attendance.js
@@ -5,8 +5,8 @@ const attendanceSchema = new Schema({
   course_id: { type: String, required: true},
   teacher_id: { type: String, required: true },
   date: {type:String,required:true},
-  is_active: {type: Boolean,default:false}
-
+  is_active: {type: Boolean,default:false},
+  duration: {type:Number, required: true},                        //new field added by yash
 },
 {
   timestamps: true
